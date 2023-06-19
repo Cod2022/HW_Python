90dd89c29a0de5ff81daa057c71af3f4fbed1e00
My Name <Me@gmail.com>
2023-06-19 17:58:38 +0400

    task1 is done

--- HW_4/task1.py
+++ HW_4/task1.py
@@ -1,1 +1,10 @@
 # Напишите функцию для транспонирования матрицы
+
+matrix = [[1, 23], [42, 5], [7, 8]]
+
+def matrix_transposition(matrix):
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+print(matrix_transposition(matrix))
+        
+
