eac42585d748dab8a140965a5711e673df3910ed
My Name <Me@gmail.com>
2023-06-20 04:46:36 +0400

    tsak3 4th commit add_money func done

--- HW4/task3.py
+++ HW4/task3.py
@@ -13,38 +13,46 @@
 # Дополнительно сохраняйте все операции поступления и снятия средств в список.
 
 INITIAL_SUM = 0
+MIN_COMMISION_LIMIT = 10
+MAX_COMISSION_LIMIT = 2000
+MULTIPLICITY_SUM = 50
+
 my_balance = 0
 
-def add_money(my_balance):
+def add_money(balance):
     input_money = int(input('Внесите сумму, кратную 50 руб: '))
-    if input_money % 50 == 0:
-        my_balance += input_money
-        print('Деньги добавлены')
-        return my_balance
+    if input_money % MULTIPLICITY_SUM == 0:
+        balance += input_money
+        print(f'Деньги добавлены.\nВаш баланс: {balance}')
+        return balance
     else:
         print('Ошибка!Внесите сумму, кратную 50 руб!')
 
     
 
-def extract_money():
+def extract_money(balance):
+    if 
     print('Деньги сняты')
 
-def exit_atm():
+def exit_atm(balance):
+    print(f'Ваш баланс: {balance}')
     print('До свидания!')
 
 
-def atm_menu(my_balance):
+def atm_menu(balance):
     while True:
-        choice = input('Выберите: \n1-пополнить счёт\n2-снять деньги\n3-выйти из системы')
+        choice = input('Выберите: \n1-пополнить счёт\n2-снять деньги\n3-выйти из системы\n')
         match choice:
             case '1':
-                add_money(my_balance)
+                balance = add_money(balance)
             case '2':
-                extract_money()
+                balance = extract_money(balance)
             case '3':
-                exit_atm()
-                return my_balance
+                exit_atm(balance)
+                break
 
-my_balance = atm_menu(my_balance)
+my_balance=atm_menu(my_balance)
+#print(my_balance)
+
 
     
