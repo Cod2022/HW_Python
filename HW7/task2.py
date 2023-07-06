5c191fd1dd729db2164560ccffeb6fe799562d66
My Name <Me@gmail.com>
2023-07-06 07:55:08 +0400

    2nd commit

--- HW7/task2.py
+++ HW7/task2.py
@@ -8,3 +8,23 @@
 
 # * принимать в качестве аргумента расширение конечного файла.
 # Шаблон переименованного файла: <original_name>_<new_name>_<position>.<new_extention>
+
+import os
+
+def file_rename(new_name, old_extension, new_extension):
+    os.chdir('HW7')
+    files_list = [_ for _ in os.listdir() if _.endswith(old_extension)]
+    for num, file in enumerate(files_list, 1):
+        # f = open(file, 'r', encoding='utf-8')
+        # line = f.readline()
+        # ext_strip = '.' + old_extension
+        # print(f'{file.strip(ext_strip)}_{str(num)}')
+        strip_old_ext = '.' + old_extension
+        new_name_extension = f'{file.strip(strip_old_ext)}_{new_name}_{str(num)}.{new_extension}'
+        os.rename(file, new_name_extension)
+
+file_rename("asd", 'txt', 'pdf')
+# for item in res:
+#     if item.endswith('.txt'):
+#             print(item)
+
