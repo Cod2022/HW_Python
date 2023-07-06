300c7f1ff1f8442243aa777f426803b213bd3de9
My Name <Me@gmail.com>
2023-07-06 08:01:21 +0400

    task2 is done

--- HW7/task2/task2.py
+++ HW7/task2/task2.py
@@ -12,19 +12,11 @@
 import os
 
 def file_rename(new_name, old_extension, new_extension):
-    os.chdir('HW7')
+    os.chdir('HW7/task2')
     files_list = [_ for _ in os.listdir() if _.endswith(old_extension)]
     for num, file in enumerate(files_list, 1):
-        # f = open(file, 'r', encoding='utf-8')
-        # line = f.readline()
-        # ext_strip = '.' + old_extension
-        # print(f'{file.strip(ext_strip)}_{str(num)}')
         strip_old_ext = '.' + old_extension
         new_name_extension = f'{file.strip(strip_old_ext)}_{new_name}_{str(num)}.{new_extension}'
         os.rename(file, new_name_extension)
 
-file_rename("asd", 'txt', 'pdf')
-# for item in res:
-#     if item.endswith('.txt'):
-#             print(item)
-
+file_rename('hello', 'txt', 'pdf')
