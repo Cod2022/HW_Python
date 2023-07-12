ddd7a904711830799804bb978dc6b8d7216cdf7e
My Name <Me@gmail.com>
2023-07-12 12:06:02 +0400

    wrote csv reading func

--- HW9/task1.py
+++ HW9/task1.py
@@ -5,15 +5,29 @@
 # Декоратор, сохраняющий переданные параметры и результаты работы функции в json файл
 import csv
 import random
+from typing import Callable
+
+NUMBER_OF_ROWS = 100
+MAX_NUMBER = 100
+NUMBER_OF_NUMBERS = 3
+
 
 
 def write_csv_nums(name: str):
-    with open(name, 'a', encoding='utf-8', newline='') as f_write:
+    with open(name, 'w', encoding='utf-8', newline='') as f_write:
         csv_write = csv.writer(f_write)
-        for _ in range(0, 100):
-            res2 = [random.randint(1, 100) for _ in range(1, 4)] 
+        for _ in range(0, NUMBER_OF_ROWS):
+            res2 = [random.randint(1, MAX_NUMBER) for _ in range(0, NUMBER_OF_NUMBERS)] 
             csv_write.writerow(res2)
+
+def read_csv_nums(name: str):
+    with open(name, 'r', encoding='utf-8', newline='') as f_read:
+        csv_read = csv.reader(f_read)
+        for line in csv_read:
+            int_list = list(map(int, line))
+            print(int_list)
 
 
 
 write_csv_nums('nums_csv.csv')
+read_csv_nums('nums_csv.csv')
